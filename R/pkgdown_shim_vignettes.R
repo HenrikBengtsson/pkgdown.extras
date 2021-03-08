ca7a49ad50374ce8e7c689a50eda0ef5ed1d15ee
Henrik Bengtsson <hb@aroma-project.org>
2021-03-07 23:17:17 -0800

    Add support for '*::selfonly' vignette engines, e.g. startup::selfonly, for '*.md' vignette files

--- R/pkgdown_shim_vignettes.R
+++ R/pkgdown_shim_vignettes.R
@@ -1,17 +1,26 @@
 #' @importFrom utils file_test 
-#' @importFrom tools pkgVignettes vignetteEngine file_path_sans_ext file_ext
+#' @importFrom tools pkgVignettes vignetteEngine vignetteInfo file_path_sans_ext file_ext
 #' @importFrom yaml write_yaml
 pkgdown_shim_vignettes <- function(path = ".", ...) {
   rule <- import_from("pkgdown", "rule")
   cat_line <- import_from("pkgdown", "cat_line")
   src_path <- import_from("pkgdown", "src_path")
   dst_path <- import_from("pkgdown", "dst_path")
+  get_vignette_metadata <- import_from("tools", ".get_vignette_metadata")
 
   stopifnot(file_test("-d", path))
   opwd <- setwd(path)
   on.exit(setwd(opwd), add = TRUE)
 
-  ## Vignettes
+  ## AD HOC: Fake 'R CMD build'
+  ## (some packages register vignette engines only when that is running)
+  rcmd <- Sys.getenv("R_CMD", NA_character_)
+  on.exit({
+    if (is.na(rcmd)) Sys.unsetenv("R_CMD") else Sys.setenv("R_CMD" = rcmd)
+  }, add = TRUE)
+  Sys.setenv("R_CMD" = "true")
+  
+  ## Find vignettes
   vignettes <- pkgVignettes(dir = ".")
   nvignettes <- length(vignettes$docs)
   if (nvignettes > 0) {
@@ -44,14 +53,23 @@ pkgdown_shim_vignettes <- function(path = ".", ...) {
         }
       })
 
+      vinfo <- vignetteInfo(file)
+      vinfo$author <- get_vignette_metadata(readLines(file), "Author")
+      author <- vinfo$author
+      yaml <- list(
+        title  = vinfo$title,
+        author = vinfo$author
+      )
+      
+      target_dir <- dirname(file)
+
       ## Special cases
       if (engine_name == "R.rsp::rsp") {
         target_file <- file_path_sans_ext(file)
-        target_dir <- dirname(target_file)
         target_ext <- file_ext(target_file)
-        
-        ## *.md.rsp
-        if (target_ext == "md") {
+
+        ## *.md.rsp -> *.md
+        if (ext == "rsp" && target_ext == "md") {
           cat_line("Weaving ", src_path(file_short))
           ## Compile *.md.rsp to *.md
           target <- local({
@@ -73,17 +91,10 @@ pkgdown_shim_vignettes <- function(path = ".", ...) {
           ## Create mockup Rmarkdown file
           rmd <- file_path_sans_ext(target)
           rmd <- paste(rmd, ".Rmd", sep = "")
-          metadata <- attr(target, "metadata")
           content <- readLines(target)
           # No longer needed
           file.remove(target)
           stopifnot(!file_test("-f", target))
-          
-          yaml <- list(
-            title  = metadata$title,
-            author = metadata$author
-          )
-
           local({
             con <- file(rmd, open = "w")
             on.exit(close(con))
@@ -103,7 +114,33 @@ pkgdown_shim_vignettes <- function(path = ".", ...) {
 
           shim_docs[kk] <- pkgdown_file
         }
+      } else if (grepl("selfonly$", engine_name)) {
+        ## *.md -> *.md
+        if (ext == "md") {
+          cat_line("Shimming ", src_path(file_short))
+          ## Create mockup Rmarkdown file
+          rmd <- file_path_sans_ext(file)
+          rmd <- paste(rmd, ".Rmd", sep = "")
+          content <- readLines(file)
+          local({
+            con <- file(rmd, open = "w")
+            on.exit(close(con))
+            cat("---\n", file = con)
+            write_yaml(yaml, file = con)
+            cat("---\n", file = con)
+            writeLines(content, con = con)
+          })
+          content <- yaml <- NULL
+
+          pkgdown_file <- file.path(target_dir, basename(rmd))
+          pkgdown_file_short <- file.path(basename(dirname(pkgdown_file)), basename(pkgdown_file))
+          cat_line("Writing ", dst_path(pkgdown_file_short))
+          stopifnot(file_test("-f", pkgdown_file))
+
+          shim_docs[kk] <- pkgdown_file
+        }
       }
+
 
       if (is.na(shim_docs[kk])) {
         cat_line("Unsupported ", sQuote(engine_name), " format ", src_path(file_short))
